16bd3258ced9eff5e5c086175585fadbc71ea1c0
William Viktorsson <wilvik05@edu.umea.se>
2022-08-31 09:21:53 +0200

    example test/users insertOne

--- first-server/src/routes/register/+server.js
+++ first-server/src/routes/register/+server.js
@@ -7,9 +7,16 @@ import * as database from '$lib/database.js';
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
     const req = await request.json();
-/* 
-    const client = await database.connect();
-    const db = client.db("test"); */
+
+    const client = await database.connect(); // Connect to the mongoDB
+    const db = client.db("test"); // select test db
+    const collection = db.collection("users");  // select users collection
+
+    if (req) {
+        // TODO: Dont just create the account. Validate that the user sent proper stuff
+        // The user doesnt already exist & passwords are provided.
+        collection.insertOne({ "username": "kalle", "password": "majarox" })
+    }
 
     const body = { "register - post": "123" }
 
@@ -28,12 +35,12 @@ export async function DELETE({ request }) {
     console.log(cookies)
 
 
-/*     const client = await database.connect();
-    const db = client.db("test"); */
+    /*     const client = await database.connect();
+        const db = client.db("test"); */
 
     const body = { "register - delete": "123" }
 
-    
+
 
 
     return json(body);

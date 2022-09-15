a39f147dbcd6665e0bd34d579523df2e309dd51a
William Viktorsson <wilvik05@edu.umea.se>
2022-09-15 22:33:22 +0200

    layout-groups-bug

--- seventh-server/src/routes/(authenticated)/settings/+page.svelte
+++
@@ -1,8 +0,0 @@
-<form method="POST" action="?/logout">
-	<button>LOGOUT</button>
-</form>
-
-
-<form method="POST" action="?/deleteaccount">
-	<button>DELETE ACCOUNT</button>
-</form>
--- seventh-server/src/routes/(unauthenticated)/register/+page.svelte
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="?/register">
-	<input type="text" name="username" />
-	<input type="passowrd" name="password" />
-	<button>REGISTER</button>
-</form>

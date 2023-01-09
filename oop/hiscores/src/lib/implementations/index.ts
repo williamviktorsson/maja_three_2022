809e4887a7c11bc4f942a04fdadb2f6c25644841
William Viktorsson <williamviktorsson@gmail.com>
2023-01-09 22:05:58 +0100

    examples of oop

--- oop/hiscores/src/lib/implementations/index.ts
+++ oop/hiscores/src/lib/implementations/index.ts
@@ -1,8 +1,12 @@
 import type { Hiscores } from "$lib/do_not_modify/hiscores";
+import { InMemoryHiscores } from "./inmemory_hiscores";
 import { MongoDBHiscores } from "./mongodb_hiscores";
+import { SQLiteHiscores } from "./sqlite_hiscores";
+
+/* TODO: ADD THE CORRECT IMPLEMENTATIONS */
 
 export const implementations: Record<string, Hiscores> = {
   mongodb: new MongoDBHiscores(),
-  sqlite: new MongoDBHiscores(),
-  inmemory: new MongoDBHiscores(),
+  sqlite: new SQLiteHiscores(),
+  inmemory: new InMemoryHiscores(),
 };

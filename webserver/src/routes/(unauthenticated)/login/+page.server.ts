a7d340916c8c86bfdbed4d3f47d86e2a50fdec0a
William Viktorsson <williamviktorsson@gmail.com>
2023-01-25 10:18:25 +0100

    broke it but made it readable

--- webserver/src/routes/(unauthenticated)/login/+page.server.ts
+++ webserver/src/routes/(unauthenticated)/login/+page.server.ts
@@ -3,69 +3,31 @@ import type { Actions, PageServerLoad } from "./$types";
 import { database } from "$lib/database";
 import * as crypto from "crypto";
 
+type ValidationReturn = { result: any; status: boolean }
+
+let test: Array<string> = []
+test.push
+
+class LoginValidation {
+
+  /**
+   * it validates, duh
+   * @param form the stuff i need
+   * @returns the stuff u get
+   */
+  static validate(form: any): ValidationReturn {
+    return { result: {}, status: true };
+  }
+}
+
 export const actions: Actions = {
   login: async ({ request, locals, cookies }) => {
     const form = await request.formData();
 
-    const username = form.get("username")?.toString();
-    const password = form.get("password")?.toString();
+    let { result, status } = LoginValidation.validate(form);
 
-    // TODO: Implement login
-    // Check if password and username
-    // exists and is correct'
-
-    if (!username) {
-      return fail(400, { username: "username missing" });
-    }
-
-    if (!password) {
-      return fail(400, { password: "password missing" });
-    }
-
-    try {
-      const result = await database.user.findFirst({
-        where: { username },
-      });
-
-      console.log(result);
-
-      if (!result) {
-        return fail(400, {
-          user: "wrong username + password combination",
-        });
-      }
-
-      const { salt, hash } = result;
-
-      const newhash = crypto
-        .pbkdf2Sync(password, salt, 1000, 64, "sha512")
-        .toString("hex");
-
-      if (newhash != hash) {
-        return fail(400, {
-          user: "wrong username + password combination",
-        });
-      }
-
-      const session = crypto.randomUUID();
-
-      const update = await database.user.update({
-        where: { id: result.id },
-        data: {
-          session,
-        },
-      });
-
-      cookies.set("session", update.session, {
-        path: "/",
-        httpOnly: true, // optional for now
-        sameSite: "strict", // optional for now
-        secure: process.env.NODE_ENV === "production", // optional for now
-        maxAge: 1200, //
-      });
-    } catch (e) {
-      console.log(e);
-      return fail(400, { server: "database connection error" });
+    if (!status) {
+      throw fail(result.code, result.data);
     }
 
     throw redirect(302, "/");

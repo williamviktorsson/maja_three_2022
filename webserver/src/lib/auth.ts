a832f43be551115c6b8df134268f2dab75fec6f9
William Viktorsson <williamviktorsson@gmail.com>
2023-01-30 21:19:34 +0100

    interface refactoring sample

--- webserver/src/lib/auth.ts
+++ webserver/src/lib/auth.ts
@@ -1,4 +1,4 @@
-import type { Auth, LoginResult } from "$lib/interfaces/auth";
+import type { Auth } from "$lib/interfaces/auth";
 import { SQLiteAuth } from "$lib/implementations/auth";
 
 export const auth:Auth = new SQLiteAuth()

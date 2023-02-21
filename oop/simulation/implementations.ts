60e909ecaea4792361376509111efb8bf6e27e8b
William Viktorsson <williamviktorsson@gmail.com>
2023-02-21 10:03:59 +0100

    implementation example

--- oop/simulation/implementations.ts
+++ oop/simulation/implementations.ts
@@ -2,6 +2,7 @@ import {
   Activity,
   Building,
   Experience,
+  Food,
   Person,
   Simulation,
   Study,
@@ -21,11 +22,46 @@ export class School implements Building {
   }
 }
 
+class William implements Person {
+  knowledge: Experience[] = [];
+  hunger: Number = 1000;
+  happiness: Number = 1000;
+  fitness: Number = 20;
+  move(location: Building): boolean {
+    throw new Error("Method not implemented.");
+  }
+  perform(activity: Activity): boolean {
+    if (activity instanceof CoffeSession) {
+      console.log("damn thats some good coffe");
+    }
+    for (let index = 0; index < activity.affects.length; index++) {
+      const element = activity.affects[index];
+
+      this.knowledge.push(element);
+    }
+    return true;
+  }
+  consume(food: Food): boolean {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class CoffeSession implements Activity {
+  affects: Experience[] = [{ name: "coffedrinking", value: 10 }];
+}
+
 export class Assignment implements Simulation {
-  people: Person[] = [];
-  activities: Activity[] = [];
+  people: Person[] = [new William()];
+  activities: Activity[] = [new CoffeSession()];
   buildings: Building[] = [];
   execute(): void {
-    console.log("oopsie woopsie I'm not implemented");
+    for (let index = 0; index < this.people.length; index++) {
+      const person = this.people[index];
+      for (let j = 0; j < this.activities.length; j++) {
+        const activity = this.activities[j];
+        person.perform(activity);
+      }
+      console.log(JSON.stringify(person));
+    }
   }
 }
